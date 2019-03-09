749a817ae4b3b33d5a155bdbb4a5391cb76ee8c2
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-03-08 23:58:03 -0300

    Melhora no print do tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -5,17 +5,17 @@ import xadrez.peças.Rei;
 import xadrez.peças.Torre;
 
 public class PartidaXadrez {
-	
+
 	private Tabuleiro tabuleiro;
-	
+
 	public PartidaXadrez() {
 		this.tabuleiro = new Tabuleiro(8, 8);
 		iniciando();
 	}
-	
+
 	public PeçaXadrez[][] getPeças() {
 		PeçaXadrez[][] matriz = new PeçaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		
+
 		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
 			for (int j = 0; j < tabuleiro.getColunas(); j++) {
 				matriz[i][j] = (PeçaXadrez) tabuleiro.peça(i, j);
@@ -23,14 +23,24 @@ public class PartidaXadrez {
 		}
 		return matriz;
 	}
-	
+
 	private void novaPeça(char coluna, int linha, PeçaXadrez peça) {
 		tabuleiro.localPeça(peça, new PosicaoXadrez(coluna, linha).paraPosicao());
 	}
-	
+
 	private void iniciando() {
-		novaPeça('b', 6, new Torre(tabuleiro, Cores.WHITE));
-		novaPeça('a', 3, new Rei(tabuleiro, Cores.BLACK));
+		novaPeça('c', 1, new Torre(tabuleiro, Cores.WHITE));
+		novaPeça('c', 2, new Torre(tabuleiro, Cores.WHITE));
 		novaPeça('d', 2, new Torre(tabuleiro, Cores.WHITE));
+		novaPeça('e', 2, new Torre(tabuleiro, Cores.WHITE));
+		novaPeça('e', 1, new Torre(tabuleiro, Cores.WHITE));
+		novaPeça('d', 1, new Rei(tabuleiro, Cores.WHITE));
+
+		novaPeça('c', 7, new Torre(tabuleiro, Cores.BLACK));
+		novaPeça('c', 8, new Torre(tabuleiro, Cores.BLACK));
+		novaPeça('d', 7, new Torre(tabuleiro, Cores.BLACK));
+		novaPeça('e', 7, new Torre(tabuleiro, Cores.BLACK));
+		novaPeça('e', 8, new Torre(tabuleiro, Cores.BLACK));
+		novaPeça('d', 8, new Rei(tabuleiro, Cores.BLACK));
 	}
 }
